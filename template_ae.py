ad689baa5b89284e4c076f4db0d1ee76d5790dc8
Mantraraj <mantraraj.dash@zs.com>
2021-07-15 18:38:42 +0530

    incremental changes

--- template_ae.py
+++ template_ae.py
@@ -7,7 +7,7 @@ import torch.utils
 import torch.nn.functional as F
 import torch.nn as nn
 import torch
-from torch.optim.lr_scheduler import CyclicLR
+from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
 
 
 torch.manual_seed(0)
@@ -15,37 +15,51 @@ plt.rcParams['figure.dpi'] = 200
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+# TODO implement multiple hidden layers
+# TODO implement batch norm and dropout
+
 
 class Encoder(nn.Module):
-    def __init__(self, input_dims, hidden_dims, latent_dims):
+    def __init__(self, input_dims, hidden_dims, latent_dims, dr_prob=0.1):
         super(Encoder, self).__init__()
         self.linear1 = nn.Linear(input_dims, hidden_dims)
         self.linear2 = nn.Linear(hidden_dims, latent_dims)
+        self.dropout_layer = nn.Dropout(dr_prob)
+        self.bn_layer = nn.BatchNorm1d(hidden_dims)
+        self.first_bn_layer = nn.BatchNorm1d(input_dims)
 
     def forward(self, x):
         x = torch.flatten(x, start_dim=1)
+        x = self.first_bn_layer(x)
         x = F.relu(self.linear1(x))
+        x = self.bn_layer(x)
+        x = self.dropout_layer(x)
+
         return self.linear2(x)
 
 
 class Decoder(nn.Module):
-    def __init__(self, input_dims, hidden_dims, latent_dims):
+    def __init__(self, input_dims, hidden_dims, latent_dims, dropout_p=0.1):
         super(Decoder, self).__init__()
         self.linear1 = nn.Linear(latent_dims, hidden_dims)
         self.linear2 = nn.Linear(hidden_dims, input_dims)
+        self.dropout_layer = nn.Dropout(dropout_p)
+        self.bn_layer = nn.BatchNorm1d(hidden_dims)
 
     def forward(self, z):
         z = F.relu(self.linear1(z))
+        z = self.bn_layer(z)
+        z = self.dropout_layer(z)
         z = torch.sigmoid(self.linear2(z))
         # return z.reshape((-1, 1, 28, 28)) ##for image
         return z
 
 
 class Autoencoder(nn.Module):
-    def __init__(self, input_dims, hidden_dims, latent_dims):
+    def __init__(self, input_dims, hidden_dims, latent_dims, dropout_p):
         super(Autoencoder, self).__init__()
-        self.encoder = Encoder(input_dims, hidden_dims, latent_dims)
-        self.decoder = Decoder(input_dims, hidden_dims, latent_dims)
+        self.encoder = Encoder(input_dims, hidden_dims, latent_dims, dropout_p)
+        self.decoder = Decoder(input_dims, hidden_dims, latent_dims, dropout_p)
 
     def forward(self, x):
         z = self.encoder(x)
@@ -60,10 +74,10 @@ def plot_losses(list_loss, list_lr):
 
     ax2 = ax1.twinx()
     x = [x+1 for x in range(len(list_lr))]
-    ax1.plot(x, list_loss, 'o-', color="blue")
-    ax2.plot(x, list_lr, 'o-', color="red")
+    ax1.plot(x, list_loss, '-', color="blue")
+    ax2.plot(x, list_lr, '-', color="red")
 
-    ax1.set_xlabel('X data')
+    ax1.set_xlabel('# Epochs')
     ax1.set_ylabel('Loss', color='blue')
     ax2.set_ylabel('LR', color='red')
 
@@ -77,9 +91,9 @@ def train(autoencoder, data, epochs=20, batch_size=1024,
 
     # learning rate scheduling
     # https://www.kaggle.com/isbhargav/guide-to-pytorch-learning-rate-scheduling
-    lr_sched = CyclicLR(opt, base_lr=learning_rate//10,
-                        max_lr=learning_rate*2,
-                        step_size_up=epochs/5, mode="triangular2")
+    lr_sched = CosineAnnealingWarmRestarts(opt, T_0=epochs//20, T_mult=2,
+                                           eta_min=learning_rate/20,
+                                           last_epoch=-1)
 
     list_loss = []
     list_lr = []
@@ -93,7 +107,8 @@ def train(autoencoder, data, epochs=20, batch_size=1024,
             loss.backward()
             opt.step()
             mean_sse += loss
-        print(f'Epoch: {epoch}; Mean SSE: {mean_sse/batch_size:0.4f}')
+        if epoch % 10 == 0:
+            print(f'Epoch: {epoch}; Mean SSE: {mean_sse/batch_size:0.4f}')
         # print(f'Learning Rate: {opt.param_groups[0]["lr"]}')
         list_loss.append(mean_sse/batch_size)
         list_lr.append(opt.param_groups[0]["lr"])
@@ -133,6 +148,9 @@ if __name__ == "main":
 
     latent_dims = 2
     input_dims = 1024
-    autoencoder = Autoencoder(input_dims, hidden_dims, latent_dims).to(device)
+    hidden_dims = 4
+    dropout_p = 0.1
+    autoencoder = Autoencoder(input_dims, hidden_dims,
+                              latent_dims, dropout_p).to(device)
 
     autoencoder = train(autoencoder, data, batch_size=1024, learning_rate=0.1)
